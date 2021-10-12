7fe4d9a9aee5435fe42f9cfaa73bc4c7d38e7613
BDFD <bdfd2005@gmail.com>
2021-10-12 01:46:00 -0400

    Update Rename Python Script

--- py_script/02_rename.py
+++ py_script/02_rename.py
@@ -1,42 +1,33 @@
 import os
 
-cwd = os.getcwd()
-print("Current working directory: {0}".format(cwd))
-folder = os.chdir(os.path.dirname()))
-print("Current working directory: {0}".format(folder))
 
-# print("Current working directory: {0}".format(cwd))
-# folder_location = os.chdir('../data/test')
-# print("Current working directory: {0}".format(folder_location))
-# for f in os.path.exists(folder_location):
-#     print(f)
-# file_name, file_ext = os.path.splitext(f)
-# print(file_name)
+def monthToNum(shortMonth):
+    return {
+        'January': 1,
+        'February': 2,
+        'March': 3,
+        'April': 4,
+        'May': 5,
+        'June': 6,
+        'July': 7,
+        'August': 8,
+        'September': 9,
+        'October': 10,
+        'November': 11,
+        'December': 12
+    }[shortMonth]
 
-# One way to do this
-# f_title, f_course, f_number = file_name.split('-')
 
-# print('{}-{}-{}{}'.format(f_number, f_course, f_title, file_ext))
-
-# Need to remove whitespace
-# f_title = f_title.strip()
-# f_course = f_course.strip()
-# f_number = f_number.strip()
-
-# Want to remove the number sign?
-# f_number = f_number.strip()[1:]
-
-# One thing I noticed about this output is that if it was sorted by filename
-# then the 1 and 10 would be next to each other. How do we fix this? One way we can fix this is to pad
-# the numbers. So instead of 1, we'll make it 01. If we had hundreds of files then this would maybe need to be 001.
-# We can do this in Python with zfill
-# f_number = f_number.strip()[1:].zfill(2)
-
-# print('{}-{}-{}{}'.format(f_number, f_course, f_title, file_ext))
-
-# You have the power to reformat in any way you see fit
-# print('{}-{}{}'.format(f_number, f_title.strip(), file_ext.strip()))
-
-# new_name = '{}-{}{}'.format(file_num, file_title, file_ext)
-
-# os.rename(fn, new_name)
+for file in os.listdir():
+    # print(file)
+    file_name, file_ext = os.path.splitext(file)
+    # print(file_name)
+    # print(file_ext)
+    f_year, f_month = file_name.split('_')
+    # print(monthToNum(month))
+    r_month = str(monthToNum(f_month)).zfill(2)
+    # print(r_month)
+    # print('{}{}{}'.format(f_year, r_month, file_ext))
+    new_name = '{}{}{}'.format(f_year, r_month, file_ext)
+    # print(new_name)
+    os.rename(file, new_name)
