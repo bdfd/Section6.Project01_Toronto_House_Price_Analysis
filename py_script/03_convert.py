d1cc0316aceebaf2cf7a4cc8fe20928b83291bfc
BDFD <bdfd2005@gmail.com>
2021-10-13 00:42:07 -0400

    2016 & 2017 House Price Done

--- py_script/03_convert.py
+++ py_script/03_convert.py
@@ -6,10 +6,10 @@ housing_price_csv_dir = '../data/trt_house_price_csv'
 
 for file in os.listdir(housing_price_pdf_dir):
     file_name = file.split('.')[0]+'t1'
-    print(file_name, 'Converted pdf to csv ...\n')
     try:
         tabula.convert_into(f'{housing_price_pdf_dir}/{file}',
                             f"{housing_price_csv_dir}/{file_name}.csv",
                             output_format="csv", pages=2)
+        print(file_name, 'Converted pdf to csv ...\n')
     except:
         print(file_name, 'not converted')
